2326e798fd0ed0697c07223107679adeae7e50ed
MacroHEX <martin18py@gmail.com>
2024-09-14 08:24:40 -0400

    feat: Se elmina cache

--- app/api/libros/route.ts
+++ app/api/libros/route.ts
@@ -1,4 +1,3 @@
-// app/api/libros/route.ts
 import {NextResponse} from 'next/server';
 import {prisma} from '@/lib/prisma';
 
@@ -9,9 +8,16 @@ interface CustomError extends Error {
 export async function GET() {
   try {
     const libros = await prisma.libro.findMany();
-    return NextResponse.json(libros, {status: 200});
+
+    const response = NextResponse.json(libros, {status: 200});
+    response.headers.set('Cache-Control', 'no-store');
+
+    return response;
   } catch (error) {
     const err = error as CustomError;
-    return NextResponse.json({message: `Error al listar los libros: ${err.message}`}, {status: 500});
+    return NextResponse.json(
+      {message: `Error al listar los libros: ${err.message}`},
+      {status: 500}
+    );
   }
 }
